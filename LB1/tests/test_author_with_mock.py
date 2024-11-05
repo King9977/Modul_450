412de9ebe6fefda38d00d51d842a7e409eeabcdc
martinst06 <mstoyanov@adobe.com>
2024-11-05 15:32:22 +0100

    unit test 1: class and separate methods

--- LB1/tests/test_author_with_mock.py
+++ LB1/tests/test_author_with_mock.py
@@ -1,13 +1,15 @@
+# Unit Test 1: Mock anstatt DB
 import unittest
 from unittest.mock import MagicMock
 from app.author import AuthorService
 
-def test_add_author_with_mock():
-    mock_connection = MagicMock()
-    mock_cursor = mock_connection.cursor.return_value
-    service = AuthorService(mock_connection)
+class TestAuthorService(unittest.TestCase):
+    def setUp(self):
+        self.mock_connection = MagicMock()
+        self.mock_cursor = self.mock_connection.cursor.return_value
+        self.service = AuthorService(self.mock_connection)
 
-    service.add_author("Kaya Senay", "2005-04-06")
-
-    mock_cursor.execute.assert_called_once_with("INSERT INTO Author (name, birth_date) VALUES (?, ?)", ("Kaya Senay", "2005-04-06"))
-    mock_connection.commit.assert_called_once()
+    def test_add_author_with_mock(self):
+        self.service.add_author("Kaya Senay", "2005-04-06")
+        self.mock_cursor.execute.assert_called_once_with("INSERT INTO Author (name, birth_date) VALUES (?, ?)", ("Kaya Senay", "2005-04-06"))
+        self.mock_connection.commit.assert_called_once()
