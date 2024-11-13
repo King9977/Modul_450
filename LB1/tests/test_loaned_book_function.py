06b5be25914fab2450718746b20e6c04960f0e24
martinst06 <mstoyanov@adobe.com>
2024-11-13 16:44:40 +0100

    better viewing

--- LB1/tests/test_loaned_book_function.py
+++ LB1/tests/test_loaned_book_function.py
@@ -9,10 +9,13 @@ class TestBookService(unittest.TestCase):
         self.service = BookService(self.mock_connection)
 
     def test_is_book_loaned(self):
-        test_cases = [(1, True),(2, False),(999, None),]
+        test_cases = [(1, True),(2, False),(999, None)]
 
         for book_id, expected_result in test_cases:
-            self.mock_cursor.fetchone.return_value = (expected_result,) if expected_result is not None else None
+            if expected_result is not None:
+                self.mock_cursor.fetchone.return_value = (expected_result,)
+            else:
+                self.mock_cursor.fetchone.return_value = None
             result = self.service.is_book_loaned(book_id)
             self.assertEqual(result, expected_result)
             self.mock_cursor.execute.assert_called_with("SELECT loaned FROM Book WHERE id = ?", (book_id,))
